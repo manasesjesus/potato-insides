5560c5370f022be2280c14cd17762e9c4a6a84b0
Manasés Jesús <manasesjesus@users.noreply.github.com>
2018-03-24 17:33:33 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
+const app  = express();
 
-express()
+app
 //  .use(express.static(path.join(__dirname, 'public')))
 //  .set('views', path.join(__dirname, 'views'))
 //  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.json('{ "hello" : "world" }'))
+  .get('/', (req, res) => res.json({ "hello" : "world" }))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+// 
+app.get("/qrcodes/costumer", function () {
+  // middleware to generate a qrcode
+  res.json({"qr" : "this is your new code"});
+});
