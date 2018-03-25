1ceacc3cfd7c7deb4c09e6642e0977a62b198738
Manasés Jesús <contact@manasesjesus.com>
2018-03-25 15:25:15 +0200

    fixing bugs in  the postback function

--- app.js
+++ app.js
@@ -179,10 +179,10 @@ function handlePostback(sender_psid, received_postback) {
     let response;
     let feedmsg;
 
-    console.log("loads the postback");
-
     // Get the payload for the postback
     let payload = received_postback.payload;
+
+console.log("payload: " + payload);
 
     // Set the response based on the postback payload
     if (payload === 'yes') {
