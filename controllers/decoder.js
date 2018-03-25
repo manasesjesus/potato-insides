ca927fed19731e9e2bfb030db2d51b8587f14446
Manasés Jesús <contact@manasesjesus.com>
2018-03-25 12:04:54 +0200

    adding decoding by url

--- controllers/decoder.js
+++ controllers/decoder.js
@@ -41,18 +41,29 @@ function decodeQRImageLocal () {
 }
 
 function decodeQRImage (url) {
+    var Jimp = require("jimp");
+    var buffer = fs.readFileSync("https://scontent-ort2-1.xx.fbcdn.net/v/t34.0-12/29138872_105103870336911_1203187849_n.jpg?_nc_cat=0&_nc_ad=z-m&_nc_cid=0&oh=0c18af2bc3abc969fd6b039ce8095e67&oe=5AB8F89A");
 
-    var qr = new QrCodeReader();
-    qr.callback = function(err, value) {
+    Jimp.read(buffer, function(err, image) {
         if (err) {
+            // TODO: handle error
+            console.log("There was an error here:");
             console.error(err);
-            // TODO handle error
-        }
-        console.log(value.result);
-        console.log(value);
-    };
-    qr.decode(url);
 
+            return;
+        }
+
+        var qr = new QrCodeReader();
+        qr.callback = function(err, value) {
+            if (err) {
+                console.error(err);
+                // TODO handle error
+            }
+            console.log(value.result);
+            console.log(value);
+        };
+        qr.decode(image.bitmap);
+    });
 }
 
 exports.decodeQRImage = decodeQRImage;
