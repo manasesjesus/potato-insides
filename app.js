32072aa16a71d6a21a0ea21fe1147846bb66cdbb
Manasés Jesús <contact@manasesjesus.com>
2018-03-25 01:13:14 +0100

    adding chatbot setup

--- app.js
+++ app.js
@@ -5,11 +5,22 @@
 const PORT = process.env.PORT || 5000;
 
 // Middlewares
-const express = require('express');
+const express = require("express");
 const app  = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
+const request = require("request");
+const Cosmic = require("cosmicjs");
+const BootBot = require("bootbot");
+require("dotenv").config();
+const chrono = require("chrono-node");
+var schedule = require("node-schedule");
+const EventEmitter = require("events").EventEmitter;
 //const fs = require('fs');
 //const path = require('path');
+
+var config = {};
+const reminders = [];
+const eventEmitter = new EventEmitter();
 
 // QR controllers
 const encoder = require("./controllers/encoder.js");
@@ -17,16 +28,17 @@ const decoder = require("./controllers/decoder.js");
 
 // Process application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
-// Landing page
-app
-//  .use(express.static(path.join(__dirname, 'public')))
-//  .set('views', path.join(__dirname, 'views'))
-  .get('/', (req, res) => res.json({ "hello" : "hooman" }))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+// Start server
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+// Define API endpoints
+app.get('/', (req, res) => res.json({ "hi" : "hooman" }));
+
 
 /**
- * 
+ *
  */
 app.get("/bizs/:id/purchases/", function (req, res) {
     let id   = req.params.id;
@@ -36,10 +48,12 @@ app.get("/bizs/:id/purchases/", function (req, res) {
     encoder.createQRImage(res, img_name);
 });
 
-// for Facebook verification
-app.get('/webhook/', function (req, res) {
-	if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
-		res.send(req.query['hub.challenge'])
-	}
-	res.send('Error, wrong token')
-})
+/**
+ *
+ */
+app.get('/webhook/', function(req, res) {
+    if (req.query['hub.verify_token'] === process.env.VERIFY_TOKEN){
+        return res.send(req.query['hub.challenge']);
+    }
+    res.send('wrong token');
+});
